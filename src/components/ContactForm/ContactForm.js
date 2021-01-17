6bab607ce4f17722477901672776d2783e7ff22f
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2021-01-17 11:44:06 +0200

    Add mask on phone input.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,6 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { useSelector, useDispatch } from 'react-redux';
+import NumberFormat from 'react-number-format';
 import { getContacts } from '../../redux/contacts/contacts-selectors';
 import { addContact } from '../../redux/contacts/contacts-actions';
 import s from './ContactForm.module.css';
@@ -8,22 +9,27 @@ const ContactForm = () => {
   const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
 
-  const { register, handleSubmit, errors } = useForm();
-  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+  const { register, control, handleSubmit, errors, reset } = useForm();
 
-  const addNewContact = ({ name, number }, e) => {
+  const addNewContact = ({ name, number }) => {
     if (
       contacts.find(
         contact => contact.name.toLowerCase() === name.toLowerCase(),
       )
     ) {
       alert(`${name} is already in contacts.`);
-      e.target.reset();
+      reset({
+        name: '',
+        number: '',
+      });
       return;
     }
 
     dispatch(addContact({ name, number }));
-    e.target.reset();
+    reset({
+      name: '',
+      number: '',
+    });
   };
 
   return (
@@ -42,22 +48,31 @@ const ContactForm = () => {
         ref={register({ required: true })}
       />
       {errors.name && <p className={s.text}>This field is required!</p>}
+
       <label className={s.label} htmlFor="number">
         Number
       </label>
-      <input
-        className={s.input}
+      <Controller
         name="number"
-        type="text"
-        ref={register({
-          required: true,
-          pattern: phoneRegExp,
-        })}
+        control={control}
+        defaultValue={false}
+        render={({ onChange, value }) => (
+          <NumberFormat
+            className={s.input}
+            type={'tel'}
+            format="+380 (##) ###-####"
+            mask="_"
+            allowEmptyFormatting
+            value={value}
+            onChange={onChange}
+          />
+        )}
       />
       {errors.number && <p className={s.text}>This field is required!</p>}
       {errors.number?.type === 'pattern' && (
         <p className={s.text}>Phone number is not valid!</p>
       )}
+
       <button className={s.btn} type="submit">
         Add contact
       </button>
